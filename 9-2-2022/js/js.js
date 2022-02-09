00e77a1f0f64f5c348ba6be4f15cabee53495f95
Mehtabrai <97447825+Mehtabrai@users.noreply.github.com>
2022-02-09 17:44:47 +0500

    second commit

---
+++ 9-2-2022/js/js.js
@@ -0,0 +1,36 @@
+// ADDITION
+var num1= 20
+var num2= 50
+
+var total=num1+num2
+console.log("total add ", total)
+
+// SUB
+var num1= 20
+var num2= 50
+
+var total=num1-num2
+console.log("total sub ", total)
+
+// MULTIPLICATION
+
+var num1= 20
+var num2= 50
+
+var total=num1*num2
+console.log("total Multiplication", total)
+
+// DIVISION
+
+var num1= 20
+var num2= 50
+
+var total=num1/num2
+console.log("total Divsion ", total)
+
+// Percentage
+var num1= 20
+var num2= 50
+
+var total=(num1/num2)*100
+console.log("total Percentage ", total)
