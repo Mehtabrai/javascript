e34df43346012452566c71ee7f69e9193ad3f0ae
Mehtabrai <97447825+Mehtabrai@users.noreply.github.com>
2022-02-11 17:43:20 +0500

    third commit

---
+++ 11-2-2022/index.js
@@ -0,0 +1,20 @@
+var Name=prompt("Enter Your Name","Name")
+
+var Age= Number(prompt("18", "18"))
+
+var City=prompt("Enter Your City Name", "Mithi")
+
+var Gender=prompt("Select Your Gender", "fe-Male")
+
+if(Age >= 18 && City === "Mithi" && Gender === "Male"){
+    console.log( " Congratulation")
+    alert("Congratulation")
+
+}
+
+else(Age < 18 && City === "diplo" && Gender === "fe-Male")
+{
+    console.log("sorry try later")
+    alert("sorry try later")
+}
+
